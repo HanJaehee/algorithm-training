e96e50d5fd22b4182878fdc43f1def2b4f0c07fe
HanJaehee <gkswogml23@sju.ac.kr>
2020-01-24 10:25:17 +0900

    add 수학2

---
+++ 수학2/소수_2581.py
@@ -0,0 +1,19 @@
+num_M = int(input())
+num_N = int(input())
+
+sum=0
+minimum=0
+for i in range(num_M, (num_N+1)):
+    count=0
+    for j in range(1, i+1):
+        if i%j==0:
+            count += 1
+    if count == 2:
+        sum+=i
+        if minimum ==0:
+            minimum = i
+if sum==0 and minimum ==0:
+    print(-1)
+else:
+    print(sum)
+    print(minimum)
