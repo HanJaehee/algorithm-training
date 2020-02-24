7b9afe0feece02c24f8a62f1ce9a6c3f212d49a0
HanJaehee <gkswogml23@sju.ac.kr>
2020-02-24 09:44:53 +0900

    add backtracking

--- 백트래킹/N과M(3).py
+++ 백트래킹/N과M(3).py
@@ -8,21 +8,15 @@ def back(depth):
         print(' '.join(map(str, arr)))
         return
     for i in range(N):
-        #print("------ %d -----" %(i))
         if check[i]:
             continue
-        check[i] = True
         arr.append(i+1)
-        #print(arr)
         back(depth+1)
-        #check[i] = False
         arr.pop()
-        #print("pop : %d" %(arr.pop()))
         
         for j in range(i+1, N):
             check[j] = False
-            #print("make False check[%d]" %(j))
-        #print(check)
+
 
 back(0)
     
